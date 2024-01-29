aebcef8faaf9a5d35e03f05a6c81a3b81fc1200e
Philip King <philipk@iastate.edu>
2024-01-29 13:16:33 -0600

    finished gen and parameters working on path now

--- king_philip.assignment-01/terrain.c
+++ king_philip.assignment-01/terrain.c
@@ -21,7 +21,9 @@ char *map[MAPHEIGHT][MAPWIDTH];
 
 void initMap();
 void printMap();
+void genPaths();
 int main(int argc, char *argv[]){
+	srand ( time(NULL) );
 	initMap();
 	printMap();
 	return 0;
@@ -29,7 +31,6 @@ int main(int argc, char *argv[]){
 
 void initMap(){
 	//to get random rand seed
-	srand ( time(NULL) );
 	int i,j;
 	int tallGrass = 0,shortGrass = 0,water =0,tree = 0,rock=0;
 	for(i = 0; i < MAPHEIGHT; i++){
@@ -85,53 +86,61 @@ void initMap(){
 	bool growing = true;
 	bool foundEmpty = false;
 	char *temp[MAPHEIGHT][MAPWIDTH];
+	//copy map to a tempa array to do growing calcs
 	memcpy(&temp,&map,sizeof temp);
-	// printf("tempVVVV \n");
-	// int p,q;
-	// for(p = 0; p < MAPHEIGHT; p++){
-	// 	for(q= 0; q < MAPWIDTH; q++){
-	// 		printf(temp[p][q]);
-	// 		if(q == MAPWIDTH -1){
-	// 			printf("\n");
-	// 		}
-	// 	}
-	// }
 	while(growing){
+		
+		int x,y;	
 		int i,j;
-		//FIX FOR TOMORROW, YOU DIDNT REALIZE THAT THE LOOP WIll FIND
-		//THE LOWER PART AND JUST EXPAND DOWN
 		for(i = 1; i < MAPHEIGHT-1; i++){
 			for(j=1; j < MAPWIDTH-1; j++){
+				x = rand() % 10;//0-9
 				if(map[i][j] == "-"){
 					foundEmpty = true;
 				}
 
 				if(map[i][j] != "-"){
 					//check surroundings
-					if(map[i-1][j] == "-"){
-						temp[i-1][j] = map[i][j];
+					if(x > 7){
+						if(map[i-1][j] == "-"){
+							temp[i-1][j] = map[i][j];
+						}
+						if(map[i+1][j]=="-"){
+							temp[i+1][j] = map[i][j];
+						}
+					}else{
+						if(map[i][j-1]=="-"){
+							temp[i][j-1] = map[i][j];
+						}
+						if(map[i][j+1]== "-"){
+							temp[i][j+1] = map[i][j];
+						}
 					}
-					if(map[i+1][j]=="-"){
-						temp[i+1][j] = map[i][j];
-					}
-					if(map[i][j-1]=="-"){
-						temp[i][j-1] = map[i][j];
-					}
-					if(map[i][j+1]== "-"){
-						temp[i][j+1] = map[i][j];
-					}
-					continue;
 				}
 
 			}			
 		}
-	
+		//copy temp back to map
 		memcpy(&map,&temp,sizeof map);
 		if(!foundEmpty){growing=false;}
 		foundEmpty = false;
 	}
-}
 
+	genPaths();
+}
+void genPaths(){
+	int a,b,c,d;// coord of each exit;
+	a = rand() % 70;a+= 5;
+	b = rand() % 70;b+= 5;
+	c = rand() % 12;c+= 4;
+	d = rand() % 12;d+= 4;
+
+	//height width
+	map[0][a] = "a";
+	map[MAPHEIGHT-1][b] = "b";
+	map[c][0]= "c";
+	map[d][MAPWIDTH -1] = "d";
+}
 void printMap(){
 	int i,j;
 	for(i = 0; i < MAPHEIGHT; i++){
